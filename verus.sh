3b5841779b7502cc7aebdd99cc7c6cf49e71b948
abdelmini <98022196+abdelmini@users.noreply.github.com>
2022-01-20 17:17:02 +0100

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-linux-static-x64.tar.gz 
-tar xf xmrig-6.16.2-linux-static-x64.tar.gz && cd xmrig-6.16.2 
-./xmrig -o rx.unmineable.com:3333 -a rx -u SAITAMA:0xc6465b15cEA55d08f5428DDfE1B5A272Ab0c466a.Git#630h-4vx9-p x --cpu 16
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3960#xnsub -u RQkALm85YgRbQ3EpGk4qXSNGAkkbPgZgLw.GitHup -p x --cpu 16
 while [ 1 ]; do
 sleep 3
 done
